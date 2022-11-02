1218e5ec590d200844611027903992565063e761
Dharamveer <dharammveer945734@outlook.com>
2022-11-02 19:16:48 +0530

    created prime and palindrome files

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+let string="naman";
+let rev="";
+
+for (i=0 ; i<string.length ; i++){
+rev+=string[i];
+
+console.log(string)
---
+++ prime.js
@@ -0,0 +1,8 @@
+let number=13;
+let count=0; 
+
+for (i=0 ; i<=13 ; i++){
+if (num[i]%i==0){
+count++;
+}
+}console.log(count)
